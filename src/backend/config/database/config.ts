71eb1385f33662438186e3ee9a9efb5c28b5f91b
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-06-29 19:36:36 -0500

    ♻️ Refactor(Prisma): La implementación de la conexión se cambia dentro de config del backend. ♻️

--- src/backend/config/database/config.ts
+++ src/backend/config/database/config.ts
@@ -1,6 +1,32 @@
-import type { PrismaClient } from '@prisma/client';
+import { PrismaClient, type Role, type UserTheme } from '@prisma/client';
+import type { Session } from 'next-auth';
 
+
+export interface IUserSession extends Session {
+    user: {
+        name?: string;
+        email?: string;
+        image?: string | null;
+        id?: string;
+        role?: Role
+        theme?: UserTheme
+        timeZone?: string;
+    }
+}
 
 export type Context = {
     prisma: PrismaClient
+    session: IUserSession | null
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    t: any
+}
+
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma
 }
