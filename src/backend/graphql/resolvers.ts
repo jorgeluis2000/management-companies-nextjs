95b40c5821617ec4b7dd883111f02ba7e1e9aefa
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-06-29 19:41:06 -0500

    ⚡️ Chore: Se agregan en algunos resolvers el uso de mensajes personalizados en los errores. ⚡️

--- src/backend/graphql/resolvers.ts
+++ src/backend/graphql/resolvers.ts
@@ -1,20 +1,36 @@
-import type { AddUserParams, DeleteUserParams, GetUserByEmailParams, GetUserParams, ListUserParams, UpdateUserParams } from "@app/utils/domain/types/UserParams";
+import type { AddUserParams, DeleteUserParams, GetUserByEmailParams, ListUserParams, UpdateUserParams } from "@app/utils/domain/types/UserParams";
 import type { Context } from "../config/database/config";
 import UserRepository from "../repositories/UserRepository";
 
 export const resolvers = {
     Query: {
-        user: async (_parent: unknown, args: GetUserParams, context: Context) => {
-            const userRepository = new UserRepository(context.prisma)
-            return await userRepository.getUser(args)
+        user: async (_parent: unknown, _args: unknown, context: Context) => {
+            if (context.session?.user.id) {
+                const userRepository = new UserRepository(context.prisma)
+                return await userRepository.getUser({ id: context.session.user.id })
+            }
+            throw new Error(context.t('QueryError.sessionAuthorization'))
         },
         users: async (_parent: unknown, args: ListUserParams, context: Context) => {
-            const userRepository = new UserRepository(context.prisma)
-            return await userRepository.listUsers(args)
+            if (context.session?.user.id && context.session.user.role !== 'ADMIN') {
+                const userRepository = new UserRepository(context.prisma)
+                return await userRepository.listUsers(args)
+            }
+            if (context.session?.user.id && context.session.user.role !== 'ADMIN') {
+                throw new Error(context.t('QueryError.sessionAuthorization'))
+            }
+            throw new Error(context.t('QueryError.notAuthenticated'))
         },
         userByEmail: async (_parent: unknown, args: GetUserByEmailParams, context: Context) => {
-            const userRepository = new UserRepository(context.prisma)
-            return await userRepository.getUserByEmail(args)
+
+            if (context.session?.user.id && context.session.user.role === 'ADMIN') {
+                const userRepository = new UserRepository(context.prisma)
+                return await userRepository.getUserByEmail(args)
+            }
+            if (context.session?.user.id && context.session.user.role !== 'ADMIN') {
+                throw new Error(context.t('QueryError.sessionAuthorization'))
+            }
+            throw new Error(context.t('QueryError.notAuthenticated'))
         }
     },
     Mutation: {
